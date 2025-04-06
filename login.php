133aed33f8bc68bb516f998eb90b3504592f8cdb
rivalscott04 <rival.biasrori@outlook.com>
2025-04-06 19:05:16 +0800

    ubah tailwind ke local

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistem Pensiun</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="/assets/js/tailwind.min.js"></script>
     <script>
         tailwind.config = {
             theme: {
