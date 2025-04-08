b3cbd5cebeab021183ae1afc69bfb1e6b0638383
rivalscott04 <rival.biasrori@outlook.com>
2025-04-09 00:19:58 +0800

    api lagi

--- api/get_pegawai_kabupaten.php
+++ api/get_pegawai_kabupaten.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../functions/logic.php';
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
 
 try {
     $pensiunManager = new PensiunManager();
